fbeeab4c65c8cbc1c3adc0887eeac7be618214ff
youhei usami <usao9110@icloud.com>
2026-08-15 10:24:20 +0400

    Update product.js

--- assets/product.js
+++ assets/product.js
@@ -2,6 +2,37 @@ const variantSelect = document.querySelector("#variant");
 const productPrice = document.querySelector("#product-price");
 const addToCartButton = document.querySelector("#add-to-cart");
 
+const sizeButtons = document.querySelectorAll('[data-option="size');
+const colorButtons = document.querySelectorAll('[data-option="color"]');
+
+const variants = JSON.perese(variantDataElement.textContent);
+
+let selectedSize = null;
+let selectedColor = null;
+
+function updateVariant() {
+  if (!selectedSize || !selectedColor) return;
+
+  const variant = variants.find((variant) => {
+    return (
+      variant.option1 === selectedSize && variant.option2 === selectedColor
+    );
+  });
+
+  if (!variant) return;
+  variantIdInput.value = variant.id;
+
+  productPrice.textContent = `Dhs.${(variant.price / 100).toFixed(2)}`;
+
+  if (variant.available) {
+    addToCartButton.disabled = false;
+    addToCartButton.textContent = "Add to Cart";
+  } else {
+    addToCartButton.disabled = true;
+    addToCartButton.textContent = "Sold out";
+  }
+}
+
 if (variantSelect && productPrice && addToCartButton) {
   variantSelect.addEventListener("change", (event) => {
     const selectedOption = event.target.options[event.target.selectedIndex];
@@ -12,7 +43,6 @@ if (variantSelect && productPrice && addToCartButton) {
 
     if (available) {
       addToCartButton.disabled = false;
-      addToCartButton.textContent = "Add to Cart";
     } else {
       addToCartButton.disabled = true;
       addToCartButton.textContent = "Sold out";
